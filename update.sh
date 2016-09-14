65ac8a4dfc7c9bfd27ad258d53b8eb326497ff7e
bifore <paul.guenezan@aliceadsl.fr>
2016-09-14 19:49:18 +0200

    add exo4

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 echo "Mise à jour en cours..."
+chmod -f 777 exo4/flag
 git stash
 git pull --rebase
 git stash pop
+chmod -f 000 exo4/flag
 echo "Mise à jour terminée !"
 echo "En cas d'erreur, contactez un encadrant"
